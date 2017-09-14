ca88e1a94a176ad8b2f16b36e9fe839467404178
Jens Wille <jens.wille@gmail.com>
2017-09-14 18:59:30 +0200

    content-script.js: Fix selection for iframes.

--- content-script.js
+++ content-script.js
@@ -2,16 +2,25 @@
    License, v. 2.0. If a copy of the MPL was not distributed with this
    file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-browser.runtime.onMessage.addListener(function(_, _, sendResponse) {
-  let selection = window.getSelection().toString();
+function getDocumentSelection(contentDocument) {
+  let selection = contentDocument.getSelection().toString();
   if (selection.length === 0) {
-    let node = document.activeElement;
-    if (node !== undefined && node.value !== undefined) {
-      if (node.selectionStart !== undefined && node.selectionEnd !== undefined) {
-        selection = node.value.substring(node.selectionStart, node.selectionEnd);
+    let node = contentDocument.activeElement;
+    if (node !== undefined) {
+      if (node.contentDocument !== undefined) {
+        selection = getDocumentSelection(node.contentDocument);
+      }
+      else if (node.value !== undefined) {
+        if (node.selectionStart !== undefined && node.selectionEnd !== undefined) {
+          selection = node.value.substring(node.selectionStart, node.selectionEnd);
+        }
       }
     }
   }
 
-  sendResponse({ selection: selection });
+  return selection;
+}
+
+browser.runtime.onMessage.addListener(function(_, _, sendResponse) {
+  sendResponse({ selection: getDocumentSelection(document) });
 });
